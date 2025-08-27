b4cd79c7e486e97366e2afac06e9623adb12e7bf
Laura Jimenez <laufjimdev@gmail.com>
2025-08-27 19:26:28 -0400

    setting up CartItem

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -9,8 +9,8 @@ function ProductList({ onHomeClick }) {
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
 
-    const handleAddToCart = (product) => {
-    dispatch(addItem(product)); // Dispatch the action to add the product to the cart (Redux action)
+    const handleAddToCart = (plant) => {
+    dispatch(addItem(plant)); // Dispatch the action to add the product to the cart (Redux action)
 
     setAddedToCart((prevState) => ({ // Update the local state to reflect that the product has been added
         ...prevState, // Spread the previous state to retain existing entries
@@ -303,7 +303,7 @@ function ProductList({ onHomeClick }) {
                             <div className="product-title">{plant.name}</div> {/* Display plant name */}
                             {/* Display other plant details like description and cost */}
                             <div className="product-description">{plant.description}</div> {/* Display plant description */}
-                            <div className="product-cost">${plant.cost}</div> {/* Display plant cost */}
+                            <div className="product-cost">{plant.cost}</div> {/* Display plant cost */}
                             <button
                                 className="product-button"
                                 onClick={() => handleAddToCart(plant)} // Handle adding plant to cart
